a054b926516ebde6683117018d27b6591923f6f7
Stefan Buhrmester <buhrmi@gmail.com>
2024-06-10 23:47:43 +0200

    use wails

---
+++ frontend/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import logo from './assets/images/logo-universal.png'
+  import {GetVersion} from '../wailsjs/go/main/App.js'
+
+  let resultText = "Please enter your name below 👇"
+  let name
+
+
+</script>
+
+<main>
+  <h1>Momentum</h1>
+  {#await GetVersion() then version}
+    {version}
+  {/await}
+</main>
+
+<style>
+
+</style>
